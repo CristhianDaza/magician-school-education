435774b299744eef6bc1947c723d97fd985e332b
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-26 15:49:28 -0500

    Mostrando imagenes de las casas y creando utils/house.js y creando archivo estilos global

--- src/components/Houses.vue
+++ src/components/Houses.vue
@@ -1,9 +1,45 @@
 <template>
-  <h1>Casas</h1>
+  <v-container>
+    <h1 class="title mb-5">Select House</h1>
+    <v-row class="mx-auto mt-5">
+      <v-col
+        cols="12"
+        sm="6"
+        md="6"
+        lg="3"
+        v-for="house in houses" :key="house.id"
+      >
+        <v-img
+          :alt="house.name"
+          :src="house.image"
+          :min-width="house.width"
+        >
+        </v-img>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import houses from '@/utils/houses.js'
+
 export default {
-  name: 'Cases'
+  name: 'Cases',
+  data () {
+    return {
+      houses
+    }
+  }
 }
 </script>
+
+<style>
+  .title {
+    text-align: center;
+    font-size: 50px;
+  }
+
+  .imageHouse {
+    min-width: 250px;
+  }
+</style>
---
+++ src/utils/houses.js
@@ -0,0 +1,28 @@
+import gryffindor from '@/assets/img/gryffindor.webp'
+import hufflepuff from '@/assets/img/hufflepuff.webp'
+import ravenclaw from '@/assets/img/ravenclaw.webp'
+import slytherin from '@/assets/img/slytherin.webp'
+
+const houses = [{
+  id: 1,
+  name: 'Gryffindor',
+  image: gryffindor,
+  width: 300
+}, {
+  id: 2,
+  name: 'Hufflepuff',
+  image: hufflepuff,
+  width: 300
+}, {
+  id: 3,
+  name: 'Ravenclaw',
+  image: ravenclaw,
+  width: 300
+}, {
+  id: 4,
+  name: 'Slytherin',
+  image: slytherin,
+  width: 400
+}]
+
+export default houses
