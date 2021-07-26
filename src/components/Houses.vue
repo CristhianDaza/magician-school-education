6b875cb6fc4ce6e164a35965da4b742546798a4c
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-26 14:51:02 -0500

    Agregando componente Houses.vue

---
+++ src/components/Houses.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Casas</h1>
+</template>
+
+<script>
+export default {
+  name: 'Cases'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="home">
     <h1>Magician School Education</h1>
+    <Houses />
   </div>
 </template>
 
 <script>
+import Houses from '@/components/Houses.vue'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Houses
+  }
 }
 </script>
