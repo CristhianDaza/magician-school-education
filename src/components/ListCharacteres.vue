5dcf06ec249f88a8925d027d1aa34e2b9ec11dbc
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-27 11:02:10 -0500

    Modificando tarjeta de characteres

--- src/components/ListCharacteres.vue
+++ src/components/ListCharacteres.vue
@@ -1,12 +1,21 @@
 <template>
-  <v-card>
-    <v-card-text>
-      <v-img
-        :src="character.image"
-      >
-      </v-img>
-    </v-card-text>
-  </v-card>
+  <router-link :to="{path: `/${this.$route.params.house}/${character.name}`}">
+    <v-card color="character">
+      <v-card-text class="imgCharacter">
+        <v-img
+          :src="character.image"
+          max-height="200"
+          max-width="200"
+        >
+        </v-img>
+      </v-card-text>
+      <v-card-title class="nameCharacter">
+        <p>
+          {{ character.name }}
+        </p>
+      </v-card-title>
+    </v-card>
+  </router-link>
 </template>
 
 <script>
@@ -20,3 +29,30 @@ export default {
   }
 }
 </script>
+
+<style>
+  .character {
+    min-height: 300px;
+    transition: all 1s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+
+  .character:hover {
+    transform: translateY(-10px);
+    box-shadow: 11px 9px 19px -4px rgba(0,0,0,0.75) !important;
+  }
+
+  .imgCharacter {
+    padding: 0;
+  }
+
+  .v-image__image.v-image__image--cover {
+    background-position: center top !important;
+  }
+
+  .nameCharacter p{
+    word-break: normal;
+    font-size: 1.5rem;
+    text-align: center;
+    padding-bottom: 0;
+  }
+</style>
