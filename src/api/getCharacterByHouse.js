23d886b05dd6558b799f883d73ad6837f2d51fb4
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-27 19:46:14 -0500

    Agregando procolo https

--- src/api/getCharacterByHouse.js
+++ src/api/getCharacterByHouse.js
@@ -1,7 +1,7 @@
 import { get } from 'axios'
 
 function getCharacterByHouse ({ house }) {
-  const URL = `http://hp-api.herokuapp.com/api/characters/house/${house}`
+  const URL = `https://hp-api.herokuapp.com/api/characters/house/${house}`
   return get(URL)
 }
 
--- src/api/getCharacterByName.js
+++ src/api/getCharacterByName.js
@@ -1,7 +1,7 @@
 import { get } from 'axios'
 
 function getCharacterByName () {
-  const URL = 'http://hp-api.herokuapp.com/api/characters/'
+  const URL = 'https://hp-api.herokuapp.com/api/characters/'
   return get(URL)
 }
 
