d2c12a9d71343390e3773863972b32479b18a3ce
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-27 20:59:37 -0500

    Eliminando loader en store

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
   <div>
-    <LoadLayout v-if="isLoading">
-      <Loader />
-    </LoadLayout>
-
-    <MainLayout v-else />
+    <MainLayout />
   </div>
 </template>
 
 <script>
-import LoadLayout from '@/layouts/LoadLayout.vue'
 import MainLayout from '@/layouts/MainLayout.vue'
-import Loader from '@/components/global/Loader.vue'
-import { mapState } from 'vuex'
 
 export default {
   name: 'app',
   components: {
-    LoadLayout,
-    MainLayout,
-    Loader
-  },
-  computed: {
-    ...mapState('loading', {
-      isLoading: 'isLoading'
-    })
+    MainLayout
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import loading from './modules/loading'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    loading
   }
 })
