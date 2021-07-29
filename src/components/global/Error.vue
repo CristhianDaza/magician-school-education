3a3d2ecafa0cbc2ee220cd0cdc59efb1a500250e
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-29 17:28:36 -0500

    Agregando state de error y componente de error

---
+++ src/components/global/Error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Error
+  </div>
+</template>
+
+<script>
+export default {
+  namae: 'Error'
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import loading from './modules/loading'
 import characters from './modules/characters'
+import error from './modules/error'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     loading,
-    characters
+    characters,
+    error
   }
 })
