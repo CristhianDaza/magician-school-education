69edb0b294614e196cce34d4e5087a108502dcfb
Cristhian Daza <cristian.d.b@hotmail.com>
2023-02-16 18:05:25 -0500

    Update getCharacterByHouse.js

--- src/api/getCharacterByHouse.js
+++ src/api/getCharacterByHouse.js
@@ -1,7 +1,7 @@
 import { get } from 'axios'
 
 function getCharacterByHouse (house) {
-  const URL = `https://hp-api.herokuapp.com/api/characters/house/${house}`
+  const URL = `https://hp-api.onrender.com/api/charactershouse/${house}`
   return get(URL)
 }
 
